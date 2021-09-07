70ed219ebb21988c1d72a23b7ff44da7470159de
AlbinoWolves <87393675+AlbinoWolves@users.noreply.github.com>
2021-09-06 20:07:34 -0400

    Update Setp 6

--- Github Testing/src/HelloWorld.java
+++ Github Testing/src/HelloWorld.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -10,17 +14,31 @@ import javafx.stage.Stage;
 public class HelloWorld extends Application {
 
     protected static int speed = 7;
-    protected static int xSize = 0;
+    protected static int GWidth = 0;
+    protected static int GLength = 0;
+    protected static int yFood = 0;
+    protected static int xFood = 0;
+    protected static boolean gameOver = false;
+    protected static Random random = new Random();
+    static Direction direction = Direction.RIGHT;
 
+
+
+    enum Direction {
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN,
+    }
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Snake Game");
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        btn.setText("Start Game'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
@@ -31,7 +49,25 @@ public class HelloWorld extends Application {
 
         StackPane root = new StackPane();
         root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.setScene(new Scene(root, 700, 700));
         primaryStage.show();
     }
+
+    public static void spawnFood() {
+        while (start == true) { 
+
+        }
+
+    }
+    
+    public static void direction() {
+
+    }
+
+    
+            
+        
+        
+        
+    
 }
